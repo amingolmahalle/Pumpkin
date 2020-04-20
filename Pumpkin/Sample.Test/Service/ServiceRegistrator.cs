2b7ab3378b47e083e15a54a88e72355797db9944
Amin <amin.golmahalle@gmail.com>
2020-04-20 19:51:32 +0430

    Add Servicescope EditUser

--- Pumpkin/Sample.Test/Service/ServiceRegistrator.cs
+++ Pumpkin/Sample.Test/Service/ServiceRegistrator.cs
@@ -1,18 +1,21 @@
 using Microsoft.Extensions.DependencyInjection;
 using Pumpkin.Contract.Registration;
 using Sample.Test.Domain.Service.Commands.AddUser;
+using Sample.Test.Domain.Service.Commands.EditUser;
 using Sample.Test.Domain.Service.Queries.GetUserById;
 using Sample.Test.Service.Commands.AddUser;
+using Sample.Test.Service.Commands.EditUser;
 using Sample.Test.Service.Queries.GetUserById;
 
 namespace Sample.Test.Service
 {
-    public class ServiceRegistrator: INeedToInstall
+    public class ServiceRegistrator : INeedToInstall
     {
         public void Install(IServiceCollection services)
         {
             services.AddScoped<IGetUserByIdService, GetUserByIdService>();
             services.AddScoped<IAddUserService, AddUserService>();
+            services.AddScoped<IEditUserService, EditUserService>();
         }
     }
 }
