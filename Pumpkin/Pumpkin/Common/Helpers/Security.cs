5bef6515b19d12b11ef39171318b10e9c8e8992a
Amin <amin.golmahalle@gmail.com>
2020-07-26 17:22:16 +0430

    Add securityHelper Class

---
+++ Pumpkin/Pumpkin/Common/Helpers/Security.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pumpkin.Common.Helpers
+{
+    public static partial class Helpers
+    {
+        public static string GetSha256Hash(string input)
+        {
+            using var sha256 = SHA256.Create();
+            var byteValue = Encoding.UTF8.GetBytes(input);
+            var byteHash = sha256.ComputeHash(byteValue);
+
+            return Convert.ToBase64String(byteHash);
+        }
+    }
+}
