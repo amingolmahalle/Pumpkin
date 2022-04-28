87523313bda3e70baa5f55ec6e7b1751a5b2fffa
amin <a.golmahale@mydigipay.com>
2022-04-29 00:48:39 +0430

    add seeddata to userConfig

--- Src/SampleWebApi/Data/EntityConfig/UserConfig.cs
+++ Src/SampleWebApi/Data/EntityConfig/UserConfig.cs
@@ -31,5 +31,40 @@ public class UserConfig : IEntityTypeConfiguration<User>
         builder.Property(c => c.NationalCode)
             .HasColumnType("char(10)")
             .HasMaxLength(10);
+        
+        SeedData(builder);
+    }
+
+    private void SeedData(EntityTypeBuilder<User> builder)
+    {
+        var users = new List<User>
+        {
+            new()
+            {
+                Id = 10001,
+                Fullname = "user 1",
+                NationalCode = "5820005546",
+                MobileNumber = "09120000001",
+                Email = "user1@gmail.com",
+                CreatedAt = new DateTime(2022, 1, 1, 10, 0, 0, DateTimeKind.Utc),
+                CreatedBy = 10001,
+                ModifiedAt = new DateTime(2022, 1, 1, 10, 0, 0, DateTimeKind.Utc),
+                ModifiedBy = 10001,
+            },
+            new()
+            {
+                Id = 10002,
+                Fullname = "user 2",
+                NationalCode = "5820005545",
+                MobileNumber = "09120000002",
+                Email = "user2@gmail.com",
+                CreatedAt = new DateTime(2022, 1, 1, 10, 0, 0, DateTimeKind.Utc),
+                CreatedBy = 10001,
+                ModifiedAt = new DateTime(2022, 1, 1, 10, 0, 0, DateTimeKind.Utc),
+                ModifiedBy = 10001,
+            }
+        };
+        
+        builder.HasData(users);
     }
 }
